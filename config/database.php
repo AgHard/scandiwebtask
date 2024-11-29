35cce9dd2ecccf63d035118e734609227639c13b
AgHard <luedice57@gmail.com>
2024-11-29 06:34:32 +0200

    visual-changes

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'driver'   => 'pdo_mysql',
-    'host'     => 'u3y93bv513l7zv6o.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',  // Corrected host
+    'host'     => 's9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',  // Corrected host
     'port'     => 3306,                                                         // Port number
-    'dbname'   => 'zoawc7t7be4sx4tp',                                           // Database name extracted from JAWSDB_URL
-    'user'     => 'b2ei55gq63tqnugm',                                           // Username extracted from JAWSDB_URL
-    'password' => 'xm1oy907ncszvr1w'                                            // Password extracted from JAWSDB_URL
+    'dbname'   => 'oseakepiqhwadlcc',                                           // Database name extracted from JAWSDB_URL
+    'user'     => 'via6isdmquwzqkv9',                                           // Username extracted from JAWSDB_URL
+    'password' => 'd2hmn3afttjdzjuc'                                            // Password extracted from JAWSDB_URL
 ];
